a92ab9553059b18924893e8ac8055890d9c4bcd4
Abhishek Gupta <abhigupta639415@gmail.com>
2026-01-13 19:18:38 +0530

    Add React setup with Parcel and package files

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Namaste-react
+
+# Parcel 
+- Dev Buil
+- Local server 
+- HMR (hot module replacement)
+- file watching algo - written in c++
+- caching - faster builds
+- img optimization 
+- Minification 
+- Bundling 
+- compress
+- consistent Hashing
+- Differential Bundling - Support older browsers
+- Diagnostic
+- Error handling 
+- HTTPS
+- Tree Shaking 
--- index.js
+++ index.js
@@ -3,11 +3,17 @@
 //     {id: "heading"},
 //     "Hello World From React !!"
 // );
+import React from "react" ;
+import ReactDOM from "react-dom/client" ;
 const parent = React.createElement(
     "div",
     {id: "parent"},
-    [React.createElement("div",{id: "child1"},[React.createElement("h1",{}, "Hello Abhishek !!"), React.createElement("h2",{}, "Hello Ankit !!")]),
-    React.createElement("div",{id: "child2"},[React.createElement("h1",{}, "Hello Abhishek !!"), React.createElement("h2",{}, "Hello Ankit !!")])]
+    [React.createElement("div",{id: "child1",key: "c1"},
+        [React.createElement("h1",{key:"g1"}, "Hello Abhishek !! 🚀 "),
+             React.createElement("h2",{key: "k1"}, "Hello Ankit !!")]),
+    React.createElement("div",{id: "child2",key: "c2"},
+        [React.createElement("h1",{key:"g2"}, "Hello Abhishek !!"), 
+            React.createElement("h2",{key: "k2"}, "Hello Ankit !!")])]
 ) ;
 console.log(parent) ; // object
 const root = ReactDOM.createRoot(document.getElementById("root"));
