d58aad09bf8d41956d35b7ee8c451d46dccd2327
Abhishek Gupta <abhigupta639415@gmail.com>
2026-01-14 21:05:30 +0530

    Creating component

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-// const heading = React.createElement(
-//     "h1",
-//     {id: "heading"},
-//     "Hello World From React !!"
-// );
+
 import React from "react" ;
 import ReactDOM from "react-dom/client" ;
-const parent = React.createElement(
-    "div",
-    {id: "parent"},
-    [React.createElement("div",{id: "child1",key: "c1"},
-        [React.createElement("h1",{key:"g1"}, "Hello Abhishek !! 🚀 "),
-             React.createElement("h2",{key: "k1"}, "Hello Ankit !!")]),
-    React.createElement("div",{id: "child2",key: "c2"},
-        [React.createElement("h1",{key:"g2"}, "Hello Abhishek !!"), 
-            React.createElement("h2",{key: "k2"}, "Hello Ankit !!")])]
-) ;
-console.log(parent) ; // object
+
+// React.createElement => Object => HTMLElement(render)
+const heading = React.createElement("h1",{id:"heading"},"This is Heading");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+const jsxHeading = (
+    <h1 id="heading">Hello JSX !! 🚀</h1>
+);
+
+
+// React component 
+const Title=()=>(
+    <h1>This is React Component 🚀</h1>
+)
+// component composition
+const Heading=()=>(
+    <div>
+         <Title/>
+        <h1>This is Another React Component 🚀</h1>
+    </div>
+)
+
+root.render(<Heading/>)
