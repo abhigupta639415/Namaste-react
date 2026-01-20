66a75d2c51abf8d98059ef5ab653bc8850cdacc1
Abhishek Gupta <abhigupta639415@gmail.com>
2026-01-20 23:28:52 +0530

    hello

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,10 @@
 import ResturantCard from "./ResturantCard"
-import { useState } from "react";
+import { useState , useEffect} from "react";
 
 
 const Body = () =>{
 
-  const [restaurants, setListOfRestaurants] = useState([
+  const [restaurants, setRestaurants] = useState([
   {
     card: {
       info: {
@@ -31,14 +31,69 @@ const Body = () =>{
       },
     },
   },
+  {
+    card: {
+      info: {
+        name: "Cafe Coffee Day",
+        city: "Delhi",
+        costForTwo: "25000",
+        costForTwoMessage: "₹250 for two",
+        cuisines: ["Coffee"],
+        avgRating: 3.9,
+        "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1Vg4ZOCkKj41ddeRMuJPW6Hh4UON7L_plRA&s",
+      },
+    },
+  },
+   {
+    card: {
+      info: {
+        name: "Blue Tokai Coffee Roasters",
+        city: "Delhi",
+        costForTwo: "30000",
+        costForTwoMessage: "₹300 for two",
+        cuisines: ["Coffee", "Beverages"],
+        avgRating: 4.7,
+        "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw41LvXnTEhtLvIH2gt8-zz36ryqjc8tFr2Q&s",
+      },
+    },
+  },
+  {
+    card: {
+      info: {
+        name: "Cafe Coffee Day",
+        city: "Delhi",
+        costForTwo: "25000",
+        costForTwoMessage: "₹250 for two",
+        cuisines: ["Coffee"],
+        avgRating: 3.9,
+        "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1Vg4ZOCkKj41ddeRMuJPW6Hh4UON7L_plRA&s",
+      },
+    },
+  },
 ]) ;
+
+// const [restaurants, setRestaurants] = useState([]) ;
+//    useEffect(()=> {
+//     fetchData() ;
+//    })
+//    const fetchData = async ()=> {
+//     const data = await fetch("https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65420&lng=77.23730&restaurantId=1295782&submitAction=ENTER")
+//     const json = await data.json() ;
+
+//     console.log(json) ;
+
+//    setRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
+//    } ;
+
+
     return(
         <div className="body">
             <button className="top-btn" 
             onClick={()=>{
                const filteredRestaurants = restaurants.filter( (res)=> res.card.info.avgRating > 4) ;
-               setListOfRestaurants(filteredRestaurants);
-             }
+               setRestaurants(filteredRestaurants);
+              }
             }
             > Top Rated Restaurant</button>
             <div className="resturant-list">
@@ -48,7 +103,6 @@ const Body = () =>{
                     restData={res}
                     />
                 )}
-                {/* <ResturantCard restName="Burger King" Cuisine="Burger , Pizza"/> */}
             </div>
         </div>
     )
